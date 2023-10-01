5c4c14ab9132cc7bbe45986eb1ae82e0ebff2dc5
Martins Gouveia <gouveiamartins6@gmail.com>
2023-10-01 22:49:22 +0100

    🎨 fix absolute path

--- components/Transactions/Transactions.tsx
+++ components/Transactions/Transactions.tsx
@@ -2,7 +2,19 @@
 
 import TransactionCard from "./TransactionCard";
 
-export default function Transactions() {
+type HistoryData = {
+	title: string;
+	date: string;
+	content: string;
+};
+
+interface TransactionProps {
+	history: HistoryData[];
+}
+
+export default function Transactions({ history }: TransactionProps) {
+	console.log(history);
+
 	return (
 		<section className="flex flex-col">
 			<h2 className="text-mauve12 flex justify-center mt-10 font-semibold text-3xl">
@@ -10,34 +22,6 @@ export default function Transactions() {
 			</h2>
 
 			<div className="container mx-auto flex flex-wrap justify-center items-center gap-6 mt-10">
-				<TransactionCard title="Tuesday, October 3, 2023 at 8:09 PM">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Dolorem, doloribus.
-				</TransactionCard>
-				<TransactionCard title="Tuesday, October 3, 2023 at 8:09 PM">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Dolorem, doloribus.
-				</TransactionCard>
-				<TransactionCard title="Tuesday, October 3, 2023 at 8:09 PM">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Dolorem, doloribus.
-				</TransactionCard>
-				<TransactionCard title="Tuesday, October 3, 2023 at 8:09 PM">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Dolorem, doloribus.
-				</TransactionCard>
-				<TransactionCard title="Tuesday, October 3, 2023 at 8:09 PM">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Dolorem, doloribus.
-				</TransactionCard>
-				<TransactionCard title="Tuesday, October 3, 2023 at 8:09 PM">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Dolorem, doloribus.
-				</TransactionCard>
-				<TransactionCard title="Tuesday, October 3, 2023 at 8:09 PM">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Dolorem, doloribus.
-				</TransactionCard>
 				<TransactionCard title="Tuesday, October 3, 2023 at 8:09 PM">
 					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
 					Dolorem, doloribus.
