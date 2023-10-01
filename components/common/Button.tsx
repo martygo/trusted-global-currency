445b72c5badcb5172ed47838248c380b08633ee5
Martins Gouveia <gouveiamartins6@gmail.com>
2023-10-01 20:10:29 +0100

    ✨ Create button component

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,3 +1,25 @@
-export default function Button() {
-	return <div>Button</div>;
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps
+	extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export default function Button({
+	children,
+	onClick,
+	className,
+	...rest
+}: ButtonProps) {
+	return (
+		<button
+			className={twMerge(
+				`bg-green4 text-green11 hover:bg-green5 items-center justify-center rounded-[4px] 
+				focus:shadow-[0_0_0_2px] focus:outline-none font-medium leading-none h-[35px] px-[15px]`,
+				className,
+			)}
+			{...rest}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
 }
