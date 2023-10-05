cc3f12573f305461a40984a77df429fe589c693a
Martins Gouveia <gouveiamartins6@gmail.com>
2023-10-05 20:03:14 +0100

    🩹 add revalidate data

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,31 @@
-"use client";
-
-import axios from "axios";
-import useSWR from "swr";
-
 import Balance from "@/components/Balance/Balance";
 
-const fetcher = (url: string) =>
-	axios
-		.get(url, {
-			headers: {
-				apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-				Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-			},
-		})
-		.then((res) => res.data);
-
-export default function HomePage() {
-	const { data: balance, error } = useSWR(
-		`https://dpysxuqiiporgeifdprm.supabase.co/rest/v1/balance?select=*`,
-		fetcher,
-		{ refreshInterval: 1000 },
-	);
-	if (error) return "Failed to load";
-	if (!balance) return "Loading...";
-
+export default async function HomePage() {
+	const data = await getData();
 	return (
 		<main className="w-full h-[330px] bg-[#F06418]">
-			<Balance balance={balance} />
+			<Balance balance={data} />
 			{/* <Transactions history={transactions} /> */}
 		</main>
 	);
+}
+
+async function getData() {
+	const res = await fetch(
+		`https://dpysxuqiiporgeifdprm.supabase.co/rest/v1/balance?select=*`,
+		{
+			method: "GET",
+			headers: {
+				apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+				Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+			},
+			next: {
+				revalidate: 0,
+			},
+		},
+	);
+
+	const balance = await res.json();
+
+	return balance;
 }
