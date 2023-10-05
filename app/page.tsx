3a14e6040d5176974dde71045106090c950a1395
Martins Gouveia <gouveiamartins6@gmail.com>
2023-10-05 20:16:03 +0100

    feat: revalidate datas

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,42 @@
+"use client";
+
+import axios from "axios";
+import useSWR from "swr";
+
 import Balance from "@/components/Balance/Balance";
+import Transactions from "@/components/Transactions/Transactions";
 
-export default async function HomePage() {
-	const data = await getData();
-	return (
-		<main className="w-full h-[330px] bg-[#F06418]">
-			<Balance balance={data} />
-			{/* <Transactions history={transactions} /> */}
-		</main>
-	);
-}
-
-async function getData() {
-	const res = await fetch(
-		`https://dpysxuqiiporgeifdprm.supabase.co/rest/v1/balance?select=*`,
-		{
-			method: "GET",
+const fetcher = (url: string) =>
+	axios
+		.get(url, {
 			headers: {
-				apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+				apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 				Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
 			},
-			next: {
-				revalidate: 0,
-			},
-		},
+		})
+		.then((res) => res.data);
+
+export default function HomePage() {
+	const { data: balance, error: balanceError } = useSWR(
+		`https://dpysxuqiiporgeifdprm.supabase.co/rest/v1/balance?select=*`,
+		fetcher,
+		{ refreshInterval: 2000 },
 	);
 
-	const balance = await res.json();
+	const { data: transactions } = useSWR(
+		`https://dpysxuqiiporgeifdprm.supabase.co/rest/v1/transactions?select=*`,
+		fetcher,
+		{ refreshInterval: 1000 },
+	);
 
-	return balance;
+	if (balanceError) return "Failed to load";
+	if (!balance) return "Loading...balance";
+	if (!transactions) return "Loading...balance";
+
+	return (
+		<main className="w-full h-[330px] bg-[#F06418]">
+			<Balance balance={balance} />
+			<Transactions history={transactions} />
+		</main>
+	);
 }
